1a3c1d1fe52cd49cc596eb320ed81ff596e6f894
Timur Matsiev <timuryn@gmail.com>
2026-04-29 09:00:15 +0000

    Update: More custom Project statuses

--- erpx/custom_scripts/project_status_aufwarten.py
+++ erpx/custom_scripts/project_status_aufwarten.py
@@ -1,44 +1,52 @@
 import frappe
 from frappe import _
 
+ALLOWED_PROJECT_STATUSES = [
+    "Completed",
+    "Cancelled",
+    "AN abgerechnet",
+    "Aktion erforderlich!",
+    "Angebot erforderlich",
+    "Angebot fertig",
+    "Auf Warten",
+    "Aufmaß erforderlich",
+    "Auftrag",
+    "Bezahlt / Abgeschlossen",
+    "Entwurf",
+    "Produktion",
+    "Rechnung erforderlich",
+    "Rechnung raus"
+]
+
 def validate_project_status(doc, method):
-    """Allow custom status 'Aufwarten' for Project doctype"""
-    allowed_statuses = ["Open", "Aufwarten", "Completed", "Cancelled"]
-    
-    if doc.status not in allowed_statuses:
-        frappe.throw(_("Invalid status. Allowed statuses are: {0}").format(", ".join(allowed_statuses)))
+    """Allow custom project statuses"""
+    if doc.status not in ALLOWED_PROJECT_STATUSES:
+        frappe.throw(_("Invalid status. Allowed statuses are: {0}").format(", ".join(ALLOWED_PROJECT_STATUSES)))
 
 def before_save_project(doc, method):
     """Store custom status flag before save"""
-    if doc.status == "Aufwarten":
-        doc._aufwarten_status = True
+    if doc.status in ALLOWED_PROJECT_STATUSES:
+        doc._custom_status = doc.status
 
 def after_save_project(doc, method):
     """Ensure custom status persists after save"""
-    if hasattr(doc, '_aufwarten_status') and doc.status != "Aufwarten":
-        frappe.db.set_value("Project", doc.name, "status", "Aufwarten")
-        frappe.db.commit()
+    if hasattr(doc, '_custom_status') and doc._custom_status in ALLOWED_PROJECT_STATUSES:
+        if doc.status != doc._custom_status:
+            frappe.db.set_value("Project", doc.name, "status", doc._custom_status)
+            frappe.db.commit()
 
 def override_project_methods():
     """Override Project validation to allow custom statuses"""
     try:
         from erpnext.projects.doctype.project.project import Project
         
-        original_validate = Project.validate
-        
         def custom_validate(self):
-            # Skip original validation to prevent status reversion
-            # original_validate(self)
-            
-            # Custom validation allowing our status
-            allowed_statuses = ["Open", "Aufwarten", "Completed", "Cancelled"]
-            if self.status not in allowed_statuses:
-                frappe.throw(_("Invalid status. Allowed statuses are: {0}").format(", ".join(allowed_statuses)))
+            if self.status not in ALLOWED_PROJECT_STATUSES:
+                frappe.throw(_("Invalid status. Allowed statuses are: {0}").format(", ".join(ALLOWED_PROJECT_STATUSES)))
         
         Project.validate = custom_validate
         
     except Exception:
         pass
 
-# Override methods when module loads
 override_project_methods()
